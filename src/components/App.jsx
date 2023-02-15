2fcf0bcaffceece867338eb1fcb48428bfceea36
Baskar <baskar.official@outlook.in>
2023-02-15 18:41:51 +0530

    1.1

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import Heading from "./Heading"
+import Footer from "./Footer"
+import Note from "./Note"
+const react = require("react")
+
+function App () {
+return(
+    <div>
+        <Heading />
+        <Note />
+        <Note /><Note /><Note /><Note /><Note /><Note /><Note /><Note /><Note /><Note /><Note />
+        <Footer />
+    </div>
+)
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const react = require("react")
+
+function Footer () {
+    const year = new Date().getFullYear();
+return(
+    <footer>
+    <p>Copyright @ {year}</p>
+    </footer>
+)
+}
+
+export default Footer
